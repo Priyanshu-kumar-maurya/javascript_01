c26172eadd7a5447f0595dddfbad68035e98ff06
Priyanshu-kumar-maurya <159711665+Priyanshu-kumar-maurya@users.noreply.github.com>
2025-04-09 03:44:30 +0000

    basic_02.js

---
+++ basic js/basic_02.js
@@ -0,0 +1,13 @@
+"use trict";//treat all js code as newsr version
+
+//alert (3+3)//we are using nodejs, not browser
+
+console.log(3+3);
+console.log ("maurya");
+
+let name = "maurya"
+let age = 19
+let isloggedIn = false
+
+console.log(typeof age)
+console.log(typeof null)
