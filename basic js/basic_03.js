e6a12be08cee110037a0ba0b25a29f24c046efef
Priyanshu-kumar-maurya <159711665+Priyanshu-kumar-maurya@users.noreply.github.com>
2025-04-09 03:45:47 +0000

    basic_03.js

---
+++ basic js/basic_03.js
@@ -0,0 +1,35 @@
+// let score = "33hj" 
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let valueInumber = Number(score)
+// console.log(typeof valueInumber);
+// console.log(valueInumber)
+
+
+
+//"33"=>33
+//"33abc" => NaN
+//true => 1; false => 0
+let isloggedIn = "kjk"
+
+let booleamIsloggedIn = Boolean(isloggedIn)
+// console.log(booleamIsloggedIn);
+
+//1 => true; => false
+// "" => false 
+// "kjk" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*********** Operastion ***********
+
+
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
